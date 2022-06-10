f4f357a9e6704dee66db80eee75d18b251a486cd
abdelrahim <abdelrahimgamal97@gmail.com>
2022-06-10 09:23:35 +0200

    fixes

--- app/src/main/java/myapp/uitest/puzzlejigsaw/SecondActivity.kt
+++ app/src/main/java/myapp/uitest/puzzlejigsaw/SecondActivity.kt
@@ -1,6 +1,7 @@
 package myapp.uitest.puzzlejigsaw
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_second.*
@@ -11,6 +12,7 @@ class SecondActivity : AppCompatActivity() {
         setContentView(R.layout.activity_second)
         setting.setOnClickListener { startActivity(Intent(this, SettingActivity::class.java)) }
         start.setOnClickListener { startActivity(Intent(this, PuzzleActivity::class.java)) }
+        mainText.setShadowLayer(4f, 4f, 4f, Color.BLACK)
 
     }
 }
